512a43a819e78af0dff7ad8c2d21c911e159dd6b
Pablo Diaz <pablo.diaz2799@users.noreply.github.com>
2018-11-23 01:29:15 -0300

    Creando y Colocando sin exito el tablero
    Intentando colocar el tablero en el panel principal de los dados ( TestDicePanel) sin exito ninguno.

--- generala-client/src/main/java/uy/edu/cei/generala/client/ui/TestDicePanel.java
+++ generala-client/src/main/java/uy/edu/cei/generala/client/ui/TestDicePanel.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.SpringLayout;
 
+import uy.edu.cei.generala.client.ui.panel.BoardPanel;
 import uy.edu.cei.generala.client.ui.panel.DicePanel;
 
 import javax.swing.JPanel;
@@ -17,6 +18,8 @@ public class TestDicePanel {
 
 	private JFrame frame;
 	private DicePanel dicePanel;
+	private BoardPanel panelj1;
+	private BoardPanel panelj2;
 
 	/**
 	 * Launch the application.
@@ -45,6 +48,7 @@ public class TestDicePanel {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 604, 498);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,6 +64,13 @@ public class TestDicePanel {
 		frame.getContentPane().add(panel);
 		SpringLayout sl_panel = new SpringLayout();
 		panel.setLayout(sl_panel);
+		
+		panelj1 = new BoardPanel();
+		sl_panel.putConstraint(SpringLayout.NORTH, dicePanel, 338, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, dicePanel, 10, SpringLayout.WEST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, dicePanel, -21, SpringLayout.SOUTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, dicePanel, 390, SpringLayout.WEST, panel);
+		panel.add(panelj1);
 		
 		dicePanel = new DicePanel();
 		sl_panel.putConstraint(SpringLayout.NORTH, dicePanel, 338, SpringLayout.NORTH, panel);
--- generala-client/src/main/java/uy/edu/cei/generala/client/ui/panel/BoardPanel.java
+++ generala-client/src/main/java/uy/edu/cei/generala/client/ui/panel/BoardPanel.java
@@ -2,6 +2,7 @@ package uy.edu.cei.generala.client.ui.panel;
 
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
+import java.awt.Color;
 
 public class BoardPanel extends JPanel {
 
@@ -9,6 +10,7 @@ public class BoardPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public BoardPanel() {
+		setBackground(Color.GRAY);
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
 
